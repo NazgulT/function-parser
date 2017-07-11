c573609467bf520c1017d10c4d3a66c7f87991af
NazgulT <naziko6a.89@gmail.com>
2017-07-11 19:27:12 +0100

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,266 @@
+from utils import *
+
+import itertools
+import random
+from collections import defaultdict
+
+# ______________________________________________________________________________
+
+def is_symbol(s):
+    """A string s is a symbol if it starts with an alphabetic char."""
+    return isinstance(s, str) and s[:1].isalpha()
+
+
+def is_var_symbol(s):
+    """A logic variable symbol is an initial-lowercase string."""
+    return is_symbol(s) and s[0].islower()
+
+
+def is_prop_symbol(s):
+    """A proposition logic symbol is an initial-uppercase string."""
+    return is_symbol(s) and s[0].isupper()
+
+
+def variables(s):
+    """Return a set of the variables in expression s.
+    >>> variables(expr('F(x, x) & G(x, y) & H(y, z) & R(A, z, 2)')) == {x, y, z}
+    True
+    """
+    return {x for x in subexpressions(s) if is_variable(x)}
+
+def is_variable(x):
+    """A variable is an Expr with no args and a lowercase symbol as the op."""
+    return isinstance(x, Expr) and not x.args and x.op[0].islower()
+
+
+def is_definite_clause(s):
+    """Returns True for exprs s of the form A & B & ... & C ==> D,
+    where all literals are positive.  In clause form, this is
+    ~A | ~B | ... | ~C | D, where exactly one clause is positive.
+    >>> is_definite_clause(expr('Farmer(Mac)'))
+    True
+    """
+    if is_symbol(s.op):
+        return True
+    elif s.op == '==>':
+        antecedent, consequent = s.args
+        return (is_symbol(consequent.op) and
+                all(is_symbol(arg.op) for arg in conjuncts(antecedent)))
+    else:
+        return False
+
+
+def parse_definite_clause(s):
+    """Return the antecedents and the consequent of a definite clause."""
+    assert is_definite_clause(s)
+    if is_symbol(s.op):
+        return [], s
+    else:
+        antecedent, consequent = s.args
+        #return conjuncts(antecedent), consequent
+        return antecedent, consequent
+
+
+# Useful constant Exprs used in examples and code:
+A, B, C, D, E, F, G, P, Q, M, x, y, z = map(Expr, 'ABCDEFGPQMxyz')
+
+def prop_symbols(x):
+    """Return a list of all propositional symbols in x."""
+    if not isinstance(x, Expr):
+        return []
+    elif is_prop_symbol(x.op):
+        return [x]
+    else:
+        return list(set(symbol for arg in x.args for symbol in prop_symbols(arg)))
+
+
+def constant_symbols(x):
+    """Return a list of all constant symbols in x."""
+    if not isinstance(x, Expr):
+        return []
+    elif is_prop_symbol(x.op) and not x.args:
+        return [x]
+    else:
+        return list({symbol for arg in x.args for symbol in constant_symbols(arg)})
+
+# Convert to Conjunctive Normal Form (CNF)
+
+
+def to_cnf(s):
+    """Convert a propositional logical sentence to conjunctive normal form.
+    That is, to the form ((A | ~B | ...) & (B | C | ...) & ...) [p. 253]
+    >>> to_cnf('~(B | C)')
+    (~B & ~C)
+    """
+    s = expr(s)
+    if isinstance(s, str):
+        s = expr(s)
+    s = eliminate_implications(s)  # Steps 1, 2 from p. 253
+    s = move_not_inwards(s)  # Step 3
+    return distribute_and_over_or(s)  # Step 4
+
+
+def eliminate_implications(s):
+    """Change implications into equivalent form with only &, |, and ~ as logical operators."""
+    s = expr(s)
+    if not s.args or is_symbol(s.op):
+        return s  # Atoms are unchanged.
+    args = list(map(eliminate_implications, s.args))
+    a, b = args[0], args[-1]
+    if s.op == '==>':
+        return b | ~a
+    elif s.op == '<==':
+        return a | ~b
+    elif s.op == '<=>':
+        return (a | ~b) & (b | ~a)
+    elif s.op == '^':
+        assert len(args) == 2  # TODO: relax this restriction
+        return (a & ~b) | (~a & b)
+    else:
+        assert s.op in ('&', '|', '~')
+        return Expr(s.op, *args)
+
+
+def move_not_inwards(s):
+    """Rewrite sentence s by moving negation sign inward.
+    >>> move_not_inwards(~(A | B))
+    (~A & ~B)"""
+    s = expr(s)
+    if s.op == '~':
+        def NOT(b):
+            return move_not_inwards(~b)
+        a = s.args[0]
+        if a.op == '~':
+            return move_not_inwards(a.args[0])  # ~~A ==> A
+        if a.op == '&':
+            return associate('|', list(map(NOT, a.args)))
+        if a.op == '|':
+            return associate('&', list(map(NOT, a.args)))
+        return s
+    elif is_symbol(s.op) or not s.args:
+        return s
+    else:
+        return Expr(s.op, *list(map(move_not_inwards, s.args)))
+
+
+def distribute_and_over_or(s):
+    """Given a sentence s consisting of conjunctions and disjunctions
+    of literals, return an equivalent sentence in CNF.
+    >>> distribute_and_over_or((A & B) | C)
+    ((A | C) & (B | C))
+    """
+    s = expr(s)
+    if s.op == '|':
+        s = associate('|', s.args)
+        if s.op != '|':
+            return distribute_and_over_or(s)
+        if len(s.args) == 0:
+            return False
+        if len(s.args) == 1:
+            return distribute_and_over_or(s.args[0])
+        conj = first(arg for arg in s.args if arg.op == '&')
+        if not conj:
+            return s
+        others = [a for a in s.args if a is not conj]
+        rest = associate('|', others)
+        return associate('&', [distribute_and_over_or(c | rest)
+                               for c in conj.args])
+    elif s.op == '&':
+        return associate('&', list(map(distribute_and_over_or, s.args)))
+    else:
+        return s
+
+
+def associate(op, args):
+    """Given an associative op, return an expression with the same
+    meaning as Expr(op, *args), but flattened -- that is, with nested
+    instances of the same op promoted to the top level.
+    >>> associate('&', [(A&B),(B|C),(B&C)])
+    (A & B & (B | C) & B & C)
+    >>> associate('|', [A|(B|(C|(A&B)))])
+    (A | B | C | (A & B))
+    """
+    args = dissociate(op, args)
+    if len(args) == 0:
+        return _op_identity[op]
+    elif len(args) == 1:
+        return args[0]
+    else:
+        return Expr(op, *args)
+
+
+_op_identity = {'&': True, '|': False, '+': 0, '*': 1}
+
+
+def dissociate(op, args):
+    """Given an associative op, return a flattened list result such
+    that Expr(op, *result) means the same as Expr(op, *args)."""
+    result = []
+
+    def collect(subargs):
+        for arg in subargs:
+            if arg.op == op:
+                collect(arg.args)
+            else:
+                result.append(arg)
+    collect(args)
+    return result
+
+
+def conjuncts(s):
+    """Return a list of the conjuncts in the sentence s.
+    >>> conjuncts(A & B)
+    [A, B]
+    >>> conjuncts(A | B)
+    [(A | B)]
+    """
+    return dissociate('&', [s])
+
+
+def disjuncts(s):
+    """Return a list of the disjuncts in the sentence s.
+    >>> disjuncts(A | B)
+    [A, B]
+    >>> disjuncts(A & B)
+    [(A & B)]
+    """
+    return dissociate('|', [s])
+
+def subst(s, x):
+    """Substitute the substitution s into the expression x.
+    >>> subst({x: 42, y:0}, F(x) + y)
+    (F(42) + 0)
+    """
+    if isinstance(x, list):
+        return [subst(s, xi) for xi in x]
+    elif isinstance(x, tuple):
+        return tuple([subst(s, xi) for xi in x])
+    elif not isinstance(x, Expr):
+        return x
+    elif is_var_symbol(x.op):
+        return s.get(x, x)
+    else:
+        return Expr(x.op, *[subst(s, arg) for arg in x.args])
+
+
+def standardize_variables(sentence, dic=None):
+    """Replace all the variables in sentence with new variables."""
+    if dic is None:
+        dic = {}
+    if not isinstance(sentence, Expr):
+        return sentence
+    elif is_var_symbol(sentence.op):
+        if sentence in dic:
+            return dic[sentence]
+        else:
+            v = Expr('v_{}'.format(next(standardize_variables.counter)))
+            dic[sentence] = v
+            return v
+    else:
+        return Expr(sentence.op,
+                    *[standardize_variables(a, dic) for a in sentence.args])
+
+
+standardize_variables.counter = itertools.count()
+
+# ______________________________________________________________________________
